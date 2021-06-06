038f37f6787b7df18c4ba029fd5f917f4b5d7f16
Adam <hailinchen123@outlook.com>
2021-06-06 12:52:56 -0700

    initial commit

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, ad
+        similique placeat, aliquid, ullam repellendus asperiores quisquam
+        voluptatum sint libero veritatis atque unde illum amet officiis
+        explicabo natus provident quod consequuntur! Ipsam soluta nam, quasi vel
+        vitae velit pariatur assumenda.
+      </p>
+    </section>
+  )
+}
+
+export default About
